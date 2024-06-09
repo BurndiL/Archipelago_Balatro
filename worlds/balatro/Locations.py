c717e8a6e0b8818c2dd735b273ab393a7ee011fc
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-09 23:47:04 +0200

    Update Locations.py
    fixed bug that assigned incorrect ids to locations

--- worlds/balatro/Locations.py
+++ worlds/balatro/Locations.py
@@ -17,8 +17,7 @@ for deck in deck_id_to_name:
     for ante in range(8):
         for stake in range(8):
             location_name = deck_id_to_name[deck] + " Ante " + str(ante+1) + " Stake " + str(stake + 1)
-            location_id = offset + prev_id
+            location_id = prev_id
             prev_id += 1
-
             balatro_location_name_to_id[location_name] = location_id
             balatro_location_id_to_name[location_id] = location_name
