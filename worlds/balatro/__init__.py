ef76de02f1236c6b172d4e53ae51580711644c08
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-20 15:25:08 +0200

    Update __init__.py

--- worlds/balatro/__init__.py
+++ worlds/balatro/__init__.py
@@ -39,6 +39,8 @@ class BalatroWorld(World):
     location_id_to_name = balatro_location_id_to_name
     location_name_to_id = balatro_location_name_to_id
 
+    shop_locations = dict()
+
     options_dataclass = BalatroOptions
     options: BalatroOptions
 
@@ -88,7 +90,7 @@ class BalatroWorld(World):
                 # print("Excluded Item: " + item_name) 
             
 
-        pool_count = len(balatro_location_name_to_id)
+        pool_count = len(balatro_location_name_to_id) + self.options.shop_items
 
         #if theres any free space fill it with filler, for example traps 
         counter = 0
@@ -137,13 +139,16 @@ class BalatroWorld(World):
 
     def create_regions(self) -> None:
         menu_region = Region("Menu", self.player, self.multiworld)
+
         self.multiworld.regions.append(menu_region)
 
         for deck in deck_id_to_name:
-            deck_region = Region(deck_id_to_name[deck], self.player, self.multiworld)
-
+            deck_name = deck_id_to_name[deck]
+            print(deck_name)
+            
+            deck_region = Region(deck_name, self.player, self.multiworld)
             for location in balatro_location_name_to_id:
-                if str(location).startswith(deck_id_to_name[deck]):
+                if str(location).startswith(deck_name):
                     location_id = balatro_location_name_to_id[location]
                     stake = balatro_location_id_to_stake[location_id]
 
@@ -156,11 +161,22 @@ class BalatroWorld(World):
 
                     deck_region.locations.append(new_location)
                     
-
+            self.multiworld.regions.append(deck_region)
             # has to have deck collected to access it
             menu_region.connect(deck_region, None, 
-                                lambda state: state.has(deck_id_to_name[deck], self.player))
+                                lambda state: state.has(deck_name, self.player))
             
+
+        # Create Shop Itmes
+        self.shop_locations = Locations.create_shop_locations(Locations.prev_id, self.options.shop_items)
+        shop_region = Region("Shop", self.player, self.multiworld)
+        for location in self.shop_locations:
+            new_location = BalatroLocation(self.player, self.shop_locations[location], location, shop_region)
+            shop_region.locations.append(new_location)
+
+        self.multiworld.regions.append(shop_region)
+        menu_region.connect(shop_region, None, lambda state: state.has_any(deck_id_to_name.items(), self.player))
+        
 
     def fill_slot_data(self) -> Dict[str, Any]:
         return self.fill_json_data()
@@ -171,6 +187,9 @@ class BalatroWorld(World):
             "ante_win_goal": self.options.ante_win_goal.value,
             "decks_win_goal": self.options.decks_win_goal.value,
             "jokers_unlock_goal": self.options.jokers_unlock_goal.value,
+            "shop_locations" : [key for key, _ in self.shop_locations.items()],
+            "minimum_price" : self.options.minimum_price,
+            "maximum_price" : self.options.maximum_price,
             "deathlink": bool(self.options.deathlink)
         }
         return base_data
