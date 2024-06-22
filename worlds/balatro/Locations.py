c82d849209c0981131b276c93e362e471df954e4
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-22 14:19:13 +0200

    Update Locations.py

--- worlds/balatro/Locations.py
+++ worlds/balatro/Locations.py
@@ -4,7 +4,6 @@ from .Options import BalatroOptions
 
 # ask what the fuck to put here lol
 offset = 5606_000
-options: BalatroOptions
 
 class BalatroLocation(Location):
     game: str = "Balatro"
@@ -16,6 +15,8 @@ balatro_location_id_to_stake = dict()
 
 prev_id = offset
 
+# deck locations
+
 for deck in deck_id_to_name:
     for ante in range(8):
         for stake in range(8):
@@ -26,16 +27,14 @@ for deck in deck_id_to_name:
             balatro_location_id_to_name[location_id] = location_name
             balatro_location_id_to_stake[location_id] = stake + 1
 
+# shop locations
+for i in range(60):
+    
+    prev_id += 1
+    location_name = "Shop Item " + str(i+1)
+    location_id = prev_id
 
-def create_shop_locations(prev_id, amount) -> dict:
-    result = dict()
-    for i in range(amount):
-        prev_id += 1
-        location_name = "Shop Item " + str(i+1)
-        location_id = prev_id
-        result[prev_id] = location_name
+    balatro_location_name_to_id[location_name] = location_id
+    balatro_location_id_to_name[location_id] = location_name
 
-        balatro_location_name_to_id[location_name] = location_id
-        balatro_location_id_to_name[location_id] = location_name
 
-    return result
